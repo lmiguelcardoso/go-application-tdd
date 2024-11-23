60016446512d59d4c50042e34d2359045d91b740
Miguel <mi_lmc@hotmail.com>
2024-11-23 19:00:35 -0300

    refactor: implement player data as store interface into the playerserver struct

--- server_test.go
+++ server_test.go
@@ -6,32 +6,56 @@ import (
 	"testing"
 )
 
-func TestGETPlayers(t *testing.T) {
-	t.Run("returns Pepper's score", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
-		response := httptest.NewRecorder()
+type StubPlayerStore struct {
+	scores map[string]int
+}
 
-		PlayerServer(response, request)
+func (s *StubPlayerStore) GetPlayerScore(name string) int {
+	score := s.scores[name]
+	return score
+}
+
+func TestGETPlayers(t *testing.T) {
+	store := StubPlayerStore{
+		map[string]int{
+			"Pepper": 20,
+			"Floyd":  10,
+		},
+	}
+	server := &PlayerServer{&store}
+
+	t.Run("returns Pepper's score", func(t *testing.T) {
+		request := createGetRequest("/players/Pepper")
+		response := httptest.NewRecorder()
+		server.ServeHTTP(response, request)
 
 		got := response.Body.String()
 		want := "20"
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertResponseBody(t, got, want)
 	})
 
 	t.Run("returns Floyd's score", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/players/Floyd", nil)
+		request := createGetRequest("/players/Floyd")
 		response := httptest.NewRecorder()
 
-		PlayerServer(response, request)
+		server.ServeHTTP(response, request)
 
 		got := response.Body.String()
 		want := "10"
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertResponseBody(t, got, want)
 	})
+}
+
+func createGetRequest(path string) *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, path, nil)
+	return req
+}
+
+func assertResponseBody(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("response body is wrong, got %q want %q", got, want)
+	}
 }
