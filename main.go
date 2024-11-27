fa3d30eef7bf5e0bc78aba8e005c576ed974a1ab
Miguel <mi_lmc@hotmail.com>
2024-11-27 14:12:34 -0300

    feat: concrete implementation leaguehandler and separating concerns

--- main.go
+++ main.go
@@ -5,21 +5,6 @@ import (
 	"net/http"
 )
 
-type InMemoryPlayerStore struct {
-	store map[string]int
-}
-
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return i.store[name]
-}
-func (i *InMemoryPlayerStore) RecordWin(name string) {
-	i.store[name]++
-}
-
-func NewInMemoryPlayerStore() *InMemoryPlayerStore {
-	return &InMemoryPlayerStore{map[string]int{}}
-}
-
 func main() {
 	server := NewPlayerServer(NewInMemoryPlayerStore())
 	log.Fatal(http.ListenAndServe(":5000", server))
